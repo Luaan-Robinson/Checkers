17e2e7109d86e4307c7faa28b41a4d7752aac7e7
Luaan-Robinson <106935625+Luaan-Robinson@users.noreply.github.com>
2026-06-06 13:26:43 +0200

    added board and pieces

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
 import  pygame
+from checkers.constants import *
+from checkers.board import *
+
+FPS = 60
+WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption('Checkers')
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    board = Board()
+
+    while run:
+         clock.tick(FPS)
+
+         for event in pygame.event.get():
+              if event.type == pygame.QUIT:
+                   run = False
+                   
+              if event.type == pygame.MOUSEBUTTONDOWN:
+                   pass
+              
+         board.draw(WIN)      
+         pygame.display.update() # Luaan, stop forgetting to call the update method.
+             
+
+    pygame.quit()               
+
+
+main()    
